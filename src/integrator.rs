4def5544d4e6b5c338756537ec2be5598a6e682e
Afnan Enayet <afnan@afnan.io>
2019-11-27 11:58:26 -0800

    Add "hittable" trait
    Add interface for intersections and collisions

--- src/integrator.rs
+++ src/integrator.rs
@@ -3,7 +3,26 @@
 
 use crate::scene::Scene;
 use crate::types::{GenFloat, GenInteger};
-use cgmath::{Point3, Vector3};
+use cgmath::Vector3;
+
+/// A struct representing the various options and parameters that can be configured for the
+/// `render` method in a particular integrator
+///
+/// We use a struct rather than simply passing in parameters to make it easier to extend the
+/// parameters that are used in the render method, as you add new parameters without breaking
+/// existing integrators.
+pub struct RenderParams<'a, I> {
+    /// The origination point of the outgoing ray
+    ///
+    /// In rendering, we trace rays or paths, and they originate at a certain point and extend
+    /// until the next collision, or go on forever.
+    p: &'a Vector3<I>,
+
+    /// A reference to the scene
+    ///
+    /// This is necessary so the render
+    scene: &'a Scene,
+}
 
 /// A trait that defines an integrator. An integrator defines the operations that are responsible
 /// for taking input data for a given pixel, and calculating the output colors at each pixel.
@@ -11,5 +30,5 @@ pub trait Integrator<I: GenInteger, F: GenFloat> {
     /// Given certain input parameters, output the color values at a particular point. This method
     /// takes a sampler to generate random numbers and a pixel location.
     // TODO(afnan) add sampler parameter as an argument
-    fn render(p: &Point3<I>, scene: &Scene) -> Vector3<F>;
+    fn render(params: RenderParams<I>) -> Vector3<F>;
 }
