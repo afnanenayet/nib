acf5c7ea68262c63f81f660d4e671cb6de9e13ab
Afnan Enayet <afnan@afnan.io>
2019-11-28 13:25:52 -0800

    Update types with new trait bounds
    Add `Ord` to the list of trait bounds for our numeric traits

--- src/types.rs
+++ src/types.rs
@@ -23,9 +23,9 @@ macro_rules! aggregate_trait {
     };
 }
 
-aggregate_trait!(GenInteger; num::NumCast, num::Integer, Copy, Sync);
-aggregate_trait!(GenFloat; num::NumCast, num::Float, Copy, Sync);
-aggregate_trait!(GenReal; num::NumCast, num::traits::real::Real, Copy, Sync);
+aggregate_trait!(GenInteger; num::NumCast, num::Integer, Copy, Sync, Ord);
+aggregate_trait!(GenFloat; num::NumCast, num::Float, Copy, Sync, Ord);
+aggregate_trait!(GenReal; num::NumCast, num::traits::real::Real, Copy, Sync, Ord);
 
 /// The particular floating point type that is going to be used in this program. If you want to
 /// switch the float type to another type, simply change the type here.
