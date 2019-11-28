d4ec83020da5b5604903d28798761087e37aedf2
Afnan Enayet <afnan@afnan.io>
2019-11-27 17:15:10 -0800

    Update parameter names, docs

--- src/integrator.rs
+++ src/integrator.rs
@@ -1,4 +1,4 @@
-//! THe integrator module defines several integrators, and also provides an interface for generic
+//! The integrator module defines several integrators, and also provides an interface for generic
 //! integrators that can be extended so that other integrators can be easily added.
 
 use crate::scene::Scene;
@@ -11,12 +11,15 @@ use cgmath::Vector3;
 /// We use a struct rather than simply passing in parameters to make it easier to extend the
 /// parameters that are used in the render method, as you add new parameters without breaking
 /// existing integrators.
+///
+/// You should pass this struct by value since it's simply a struct of references to objects, which
+/// is pretty cheap.
 pub struct RenderParams<'a, I> {
     /// The origination point of the outgoing ray
     ///
     /// In rendering, we trace rays or paths, and they originate at a certain point and extend
     /// until the next collision, or go on forever.
-    p: &'a Vector3<I>,
+    origin: &'a Vector3<I>,
 
     /// A reference to the scene
     ///
@@ -29,6 +32,5 @@ pub struct RenderParams<'a, I> {
 pub trait Integrator<I: GenInteger, F: GenFloat> {
     /// Given certain input parameters, output the color values at a particular point. This method
     /// takes a sampler to generate random numbers and a pixel location.
-    // TODO(afnan) add sampler parameter as an argument
     fn render(params: RenderParams<I>) -> Vector3<F>;
 }
