039f64f3325e1734f681a4c1323816dc7530bc9a
Afnan Enayet <afnan@afnan.io>
2019-11-30 16:15:17 -0800

    Update integrator parameters
    * Add a reference to the sampler
    * Update comments

--- src/integrator.rs
+++ src/integrator.rs
@@ -1,8 +1,11 @@
 //! The integrator module defines several integrators, and also provides an interface for generic
 //! integrators that can be extended so that other integrators can be easily added.
 
-use crate::scene::Scene;
-use crate::types::{GenFloat, GenInteger, Ray};
+use crate::{
+    sampler::Sampler,
+    scene::Scene,
+    types::{GenFloat, Ray},
+};
 use cgmath::Vector3;
 
 /// A struct representing the various options and parameters that can be configured for the
@@ -14,21 +17,26 @@ use cgmath::Vector3;
 ///
 /// You should pass this struct by value since it's simply a struct of references to objects, which
 /// is pretty cheap.
-pub struct RenderParams<'a, T, S> {
+pub struct RenderParams<'a, T: GenFloat> {
     /// The outgoing ray
     ///
     /// In rendering, we trace rays or paths, and they originate at a certain point and extend
     /// until the next collision, or go on forever.
-    origin: &'a Ray<T>,
+    pub origin: &'a Ray<T>,
 
     /// A reference to the scene
-    scene: &'a Scene<S>,
+    pub scene: &'a Scene<T>,
+
+    /// A reference to the sampler to use with the integrator
+    pub sampler: &'a mut dyn Sampler<T>,
 }
 
 /// A trait that defines an integrator. An integrator defines the operations that are responsible
 /// for taking input data for a given pixel, and calculating the output colors at each pixel.
-pub trait Integrator<I: GenInteger, F: GenFloat> {
-    /// Given certain input parameters, output the color values at a particular point. This method
-    /// takes a sampler to generate random numbers and a pixel location.
-    fn render(params: RenderParams<I, F>) -> Vector3<F>;
+pub trait Integrator<T: GenFloat> {
+    /// Calculate the color value for a particular pixel, given a reference to the scene.
+    ///
+    /// Given certain input parameters this method calculates the color values at a particular
+    /// point.
+    fn render(params: RenderParams<T>) -> Vector3<T>;
 }
