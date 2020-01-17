b7962a19a8fc0f58db3a94522c617e2d6a5fae4d
Afnan Enayet <afnan@afnan.io>
2020-01-17 00:40:02 -0800

    Add TOML dependency, cli args
    * Add TOML dependency since scene descriptions will be in TOML
    * Add CLI args to main app

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,28 @@ mod sampler;
 mod scene;
 mod types;
 
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+/// An oxidized renderer
+#[derive(StructOpt, Debug)]
+#[structopt(author = "Afnan Enayet")]
+struct Args {
+    /// The path to the file describing the scene
+    pub scene: PathBuf,
+
+    /// If enabled, this flag will hide the progress bar. The progress bar is ordinarily displayed
+    /// to STDERR.
+    #[structopt(short = "p", long = "hide-progress")]
+    pub hide_progress: bool,
+
+    /// The number of threads to use in the renderer
+    #[structopt(short, long)]
+    pub threads: Option<u32>,
+}
+
 fn main() {
+    let args = Args::from_args();
     let sampler = sampler::Random::default();
     let camera: camera::Pinhole<f32> = camera::Pinhole::default();
 }
