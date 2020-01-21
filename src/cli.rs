46755c2216c234887160bfea0f55cb5598043c8e
Afnan Enayet <afnan@afnan.io>
2020-01-20 18:29:52 -0800

    Update CLI interface, support multiple file types
    * Update the CLI interface with more arguments, anticipating real world
      usage
    * Support multiple file types for the scene description

--- src/cli.rs
+++ src/cli.rs
@@ -3,7 +3,9 @@
 
 use crate::scene::*;
 use anyhow::{self, format_err};
-use serde_json;
+use json5;
+use ron;
+use serde_yaml;
 use std::{fs::File, io::Read, path::PathBuf};
 use structopt::StructOpt;
 
@@ -14,6 +16,17 @@ pub struct Args {
     /// The path to the file describing the scene
     pub scene: PathBuf,
 
+    /// The file type of the scene description file. If this is not supplied, the application will
+    /// attempt to guess the file type from the file extension. Valid values are: "ron", "yaml",
+    /// "yml", "json".
+    #[structopt(short, long)]
+    pub filetype: Option<String>,
+
+    /// Only parse the scene file and do nothing else. This can be helpful when trying to construct
+    /// scene files to ensure that they conform to the spec and will be deserialized properly.
+    #[structopt(short = "r", long)]
+    pub only_parse: bool,
+
     /// If enabled, this flag will hide the progress bar. The progress bar is ordinarily displayed
     /// to STDERR.
     #[structopt(short = "p", long = "hide-progress")]
@@ -23,6 +36,11 @@ pub struct Args {
     /// to the number of CPUs detected.
     #[structopt(short, long)]
     pub threads: Option<u32>,
+
+    /// The filename of the output file. If this is not provided it will default to "out.png". The
+    /// output file type is inferred from the filename.
+    #[structopt(short, long)]
+    pub output: Option<String>,
 }
 
 /// Parse the input scene file based on the file extension
@@ -30,29 +48,39 @@ pub struct Args {
 /// Because I am allowing multiple types of files for the scene files (e.g. JSON or YAML), and also
 /// may change the formats in the future, we have a wrapper method to parse the scene file into a
 /// scene struct. Right now it attempts to determine the file type from the path's extension.
-pub fn dispatch_scene_parse(path: &PathBuf) -> anyhow::Result<Scene<f32>> {
+///
+/// Right now we support the following filetypes for the scene description:
+/// - yaml
+/// - json
+/// - json5 (which is a superset of json)
+/// - RON (Rusty Object Notation)
+///
+/// I would recommend using RON since it's the most expressive give that we are using Rust data
+/// structures, and it has full support for all of serde's data types, which is what we're using to
+/// serialize.
+pub fn dispatch_scene_parse(path: &PathBuf, ext: Option<&str>) -> anyhow::Result<Scene<f32>> {
     if !path.exists() {
         return Err(format_err!(
             "Path to scene file \"{}\" does not exist",
             path.to_string_lossy()
         ));
     }
+    let mut file_str = String::new();
+    File::open(path)?.read_to_string(&mut file_str)?;
+    let candidate_ext: Option<&str> = match ext {
+        None => path.extension().map(|x| x.to_str().unwrap_or_default()),
+        Some(x) => Some(x),
+    };
 
-    match path.extension() {
+    match candidate_ext {
         None => Err(format_err!(
             "Could not determine the filetype of the scene file"
         )),
-        Some(ext) => {
-            let ext = ext.to_str().unwrap_or_default();
-            let mut scene_file = File::open(path)?;
-            let mut buffer = String::new();
-            scene_file.read_to_string(&mut buffer)?;
-            match ext {
-                "json" => serde_json::from_str(&buffer).map_err(|x| x.into()),
-                _ => Err(format_err!(
-                    "Could not determine the filetype of the scene file"
-                )),
-            }
-        }
+        Some(ext) => match ext {
+            "ron" => ron::de::from_str(&file_str).map_err(|x| x.into()),
+            "json" => json5::from_str(&file_str).map_err(|x| x.into()),
+            "yaml" | "yml" => serde_yaml::from_str(&file_str).map_err(|x| x.into()),
+            _ => Err(format_err!("Filetype \"{}\" is not supported", ext)),
+        },
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,12 @@ mod scene;
 mod types;
 
 use crate::scene::Scene;
-use std::error::Error;
 use cli::{dispatch_scene_parse, Args};
+use std::error::Error;
 use structopt::StructOpt;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::from_args();
-    let scene: Scene<f32> = dispatch_scene_parse(&args.scene)?;
+    let scene: Scene<f32> = dispatch_scene_parse(&args.scene, args.filetype.as_deref())?;
     Ok(())
 }
