112f393847dad3b77289e5f7624a73f2521fec93
Afnan Enayet <afnan@afnan.io>
2020-02-24 06:22:39 -0800

    Disable jemalloc for windows
    * Update build manifests and main file to only configure jemalloc on
      non-windows targets
    * Bump version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[cfg(not(target_os = "windows"))]
 #[global_allocator]
 static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
 
@@ -22,9 +23,11 @@ use crate::{
 };
 use anyhow;
 use cli::{dispatch_scene_parse, Args};
-use jemallocator;
 use std::path::Path;
 use structopt::StructOpt;
+
+#[cfg(not(target_os = "windows"))]
+use jemallocator;
 
 fn main() -> anyhow::Result<()> {
     let args = Args::from_args();
