c645a8b58a3db789419d76a6068f2ee7f1902f07
Afnan Enayet <afnan@afnan.io>
2020-03-08 17:57:02 -0700

    Update the script to push docs
    The script now unstages any other changes and commits changes to the
    docs as well as pushes them in a separate automated commit

--- publish_docs.bash
+++ publish_docs.bash
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
 # Generate documentation with Cargo and copy the generated HTML to the docs/
-# directory
+# directory. This will unstage any staged changes to keep the commit for
+# updating the documentation modular.
 
+git reset -- ./
 mkdir -p docs/
 cargo doc
 cp -r target/doc/* docs/
 echo "<head><meta http-equiv='refresh' content='0;URL=https://afnan.io/nib/nib/index.html'></head>" > docs/index.html
+git add docs
+git commit -m "[AUTOMATED] Update docs"
+git push
