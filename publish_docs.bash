9e0bd798035212d8963bda9409163f9621bea334
Afnan Enayet <afnan@afnan.io>
2020-03-08 17:58:36 -0700

    Revert "Update the script to push docs"
    This reverts commit c645a8b58a3db789419d76a6068f2ee7f1902f07. The script
    causes a infinite loop because it tries to push things to the repo,
    which in turn invokes the script

--- publish_docs.bash
+++ publish_docs.bash
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
 # Generate documentation with Cargo and copy the generated HTML to the docs/
-# directory. This will unstage any staged changes to keep the commit for
-# updating the documentation modular.
+# directory
 
-git reset -- ./
 mkdir -p docs/
 cargo doc
 cp -r target/doc/* docs/
 echo "<head><meta http-equiv='refresh' content='0;URL=https://afnan.io/nib/nib/index.html'></head>" > docs/index.html
-git add docs
-git commit -m "[AUTOMATED] Update docs"
-git push
