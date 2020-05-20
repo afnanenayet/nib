1ebf50f0e5636340a14f3198c392fcefe4c0433e
Afnan Enayet <afnan@afnan.io>
2020-05-20 12:23:40 -0400

    Remove script to publish docs
    Since we're no longer using the docs folder on the master branch to
    publish documentation, we don't need this script anymore

--- publish_docs.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Generate documentation with Cargo and copy the generated HTML to the docs/
-# directory
-
-mkdir -p docs/
-cargo doc
-cp -r target/doc/* docs/
-echo "<head><meta http-equiv='refresh' content='0;URL=https://afnan.io/nib/nib/index.html'></head>" > docs/index.html
